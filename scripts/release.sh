72f83764741c1a590953103d0024c9e09e3030bc
bytehow <omar@byte.how>
2021-06-01 18:13:55 -0700

    Final commit

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# ssh root@covid.unfinished.fun /root/scripts/server_release.sh
-ssh root@128.199.6.143 /root/scripts/server_release.sh
+ssh root@covid.unfinished.fun /root/scripts/server_release.sh
